8c9ff56215aec1c87777f6c324266202b5bd80ca
Sri Raj Dutt Badam <srirajdutt@gmail.com>
2011-04-10 02:07:35 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lecpool::Application.config.secret_token = 'd947e0dd2d7c6c663a3289ba28208c3773b5e75c220d57d47f0588d8959a3e542d1d56fe1f27b28876a47015a3626ff13baecc5512f48258c770c04e44f533d0'
